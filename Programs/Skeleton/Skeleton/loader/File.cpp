42e27e45478a343d6b32039a98b65de18e5c6165
Németh Botond <nemethbotond.sopron@gmail.com>
2024-03-05 23:20:38 +0100

    Minor changes
    Signed-off-by: Németh Botond <nemethbotond.sopron@gmail.com>

---
+++ Programs/Skeleton/Skeleton/loader/File.cpp
@@ -0,0 +1,25 @@
+﻿#include "File.h"
+
+#include <fstream>
+#include <iostream>
+
+std::string File::readFile(std::string filepath) {
+    return readFile(filepath.data());
+}
+
+std::string File::readFile(const char* filePath) {
+    std::string content;
+    std::ifstream fileStream(filePath);
+    if (!fileStream.is_open()) {
+        std::cerr << "could not read file: " << filePath << '\n';
+        throw "File does not exist!";
+    }
+    std::string line;
+    while (!fileStream.eof()) {
+        std::getline(fileStream, line);
+        content.append(line + "\n");
+    }
+
+    fileStream.close();
+    return content;
+}
---
+++ Programs/Skeleton/Skeleton/loader/File.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include <string>
+
+class File
+{
+    File() = default;
+public:
+    static std::string readFile(std::string filepath);
+    static std::string readFile(const char* filePath);
+};
